923c61adfbffac521e052ffa12f4cb7d1492afad
Gvants <122465852+usarnameMe@users.noreply.github.com>
2023-09-02 20:35:06 +0400

    Update EditAccount.jsx

--- src/screens/EditAccount.jsx
+++ src/screens/EditAccount.jsx
@@ -1,84 +1,111 @@
 import React, { useState, useEffect, useContext } from "react";
-import { TextInput, View, Text } from "react-native";
+import {
+  TextInput,
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import styled from "styled-components";
 import AvatarPicker from "./AvatarPicker.";
 import { MaterialIcons } from "@expo/vector-icons";
 import { UserContext, UpdateUserContext } from "../UserProvider";
-import { KeyboardAvoidingView, Platform, _ScrollView } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
 
 const Container = styled.View`
   flex: 1;
-  padding: 20px;
-  background-color: #fff;
+  padding: 24px;
+  background-color: #f5f5f5;
 `;
 
 const Header = styled.Text`
-  font-size: 24px;
+  font-size: 32px;
   margin-bottom: 20px;
+  font-weight: bold;
+  color: #333;
 `;
 
 const SectionHeader = styled.Text`
-  font-size: 18px;
-  font-weight: bold;
-  margin-top: 30px;
+  font-size: 20px;
+  font-weight: 600;
+  margin-top: 40px;
   margin-bottom: 20px;
+  color: #333;
 `;
 
 const EditIconWrapper = styled.View`
   position: absolute;
   left: 87px;
   top: 120px;
-  background-color: white;
+  background-color: #f5f5f5;
   border-radius: 50px;
-  padding: 5px;
+  padding: 8px;
+  border: 1px solid #ccc;
 `;
 
 const EditIcon = styled(MaterialIcons)`
-  color: #000;
-  background-color: white;
-  padding: 5px;
+  color: #333;
 `;
 
 const Field = styled.View`
-  margin-bottom: 20px;
+  margin-bottom: 25px;
   flex-direction: row;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
 `;
 
 const FieldText = styled.Text`
   font-size: 18px;
-  line-height: 30px;
-  font-weight: 600;
+  line-height: 32px;
+  font-weight: 500;
+  color: #333;
 `;
 
 const FieldLine = styled.View`
-  border-bottom-width: 2px;
+  border-bottom-width: 1px;
   border-bottom-color: #ccc;
-  margin-bottom: 10px;
+  margin-bottom: 12px;
 `;
 
 const InputField = styled.TextInput`
   font-size: 16px;
-  line-height: 30px;
-  padding: 5px;
+  padding: 8px 12px;
+  margin: 5px;
   flex: 1;
-  align-items: center;
-  justify-content: center;
-  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  background-color: #fff;
 `;
 
 const SaveButton = styled.TouchableOpacity`
-  background-color: #080a08;
-  color: white;
-  padding: 15px;
-  margin: 10px;
-  margin-top: 20px;
-  text-align: center;
+  background-color: #333;
+  padding: 16px;
+  margin-top: 24px;
   align-items: center;
   justify-content: center;
-  border-radius: 15px;
+  border-radius: 12px;
+`;
+
+const Snackbar = styled.View`
+  position: absolute;
+  top: 20px;
+  left: 0;
+  right: 0;
+  align-items: center;
+`;
+
+const SnackbarContent = styled.View`
+  background-color: #43a047;
+  padding: 16px;
+  border-radius: 4px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SnackbarText = styled.Text`
+  color: white;
+  margin-left: 8px;
 `;
 
 function EditAccount() {
@@ -89,6 +116,8 @@ function EditAccount() {
   const [name, setName] = useState(user?.name || "");
   const [phone, setPhone] = useState(user?.phone || "");
   const [email, setEmail] = useState(user?.email || "");
+  const [snackBarVisible, setSnackBarVisible] = useState(false);
+  const [snackBarMessage, setSnackBarMessage] = useState("");
 
   const saveChanges = () => {
     if (updateUser) {
@@ -98,6 +127,9 @@ function EditAccount() {
         phone,
         email,
       });
+      setSnackBarMessage("Account updated successfully!");
+      setSnackBarVisible(true);
+      setTimeout(() => setSnackBarVisible(false), 3000);
     }
   };
 
@@ -123,7 +155,7 @@ function EditAccount() {
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={{ flex: 1 }}
     >
-      <ScrollView>
+      <ScrollView style={{ flex: 1 }}>
         <Container>
           <Header>Account info</Header>
           <AvatarPicker uri={avatarUri} onAvatarChange={setAvatarUri} />
@@ -137,12 +169,12 @@ function EditAccount() {
           </Field>
           <FieldLine />
           <Field>
-            <FieldText>Phone: </FieldText>
+            <FieldText>Phone:</FieldText>
             <InputField value={phone} onChangeText={(text) => setPhone(text)} />
           </Field>
           <FieldLine />
           <Field>
-            <FieldText>Email: </FieldText>
+            <FieldText>Email:</FieldText>
             <InputField value={email} onChangeText={(text) => setEmail(text)} />
           </Field>
           <FieldLine />
@@ -151,6 +183,14 @@ function EditAccount() {
           </SaveButton>
         </Container>
       </ScrollView>
+      {snackBarVisible && (
+        <Snackbar>
+          <SnackbarContent>
+            <MaterialIcons name="check-circle" size={24} color="white" />
+            <SnackbarText>{snackBarMessage}</SnackbarText>
+          </SnackbarContent>
+        </Snackbar>
+      )}
     </KeyboardAvoidingView>
   );
 }
